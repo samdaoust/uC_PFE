67c8d6c9740e7c2fff295f084a23f7c46d9df28b
samueldaoust <samueldaoust@192.168.2.1>
2019-02-16 16:50:33 -0500

    first commit

---
+++ main.c
@@ -0,0 +1,23 @@
+/*
+ * File:   main.c
+ * Author: samueldaoust
+ *
+ * Created on February 16, 2019, 4:31 PM
+ */
+
+
+#include <xc.h>
+
+// set Config bits
+#pragma config FOSC=INTOSC
+
+//TODO set config bit
+
+//Definitions
+
+//---------------------------------------------------------------------
+//-------------------------Main Function-------------------------------
+//---------------------------------------------------------------------
+void main(void) {
+    return;
+}
